ec990eb0401b14f1a1d9c65415d0c47395c7267e
Abdullah Çobaner <149663398+tuzluzaza@users.noreply.github.com>
2024-05-13 22:48:25 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+    $kuladi = $_POST["exampleInputEmail1"];
+    $sifre = $_POST["exampleInputPassword1"];
+
+    $bkuladi = "abdullah.cobaner@ogr.sakarya.edu.tr";
+    $bsifre = "asddsa123";
+
+    if ($kuladi == $bkuladi && $sifre == $bsifre) {
+        // Giriş başarılı ise yönlendir
+        header("Location: login.php");
+        exit; // Yönlendirmeden sonra kodun devam etmemesi için
+    } else {
+        // Eşleşme yoksa hata mesajı göster
+        echo "E-posta adresi veya şifre hatalı. Lütfen tekrar deneyin.";
+    }
+?>
